68b58381cb3405d86364e3aa14f64c2885dbf62b
Ziyao Zhang <ziyaointl@gmail.com>
2017-07-21 15:38:39 -0500

    Add otsu(img)

--- imutil.py
+++ imutil.py
@@ -8,3 +8,7 @@ def show_img(img):
 
 def to_gray_scale(img):
     return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+def otsu(img):
+    ret2, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    return img
