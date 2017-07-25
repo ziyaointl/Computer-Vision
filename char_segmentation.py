d409d1576045f2a00eb37ad9cce50bd99f548115
Ziyao Zhang <ziyaointl@gmail.com>
2017-07-24 21:10:40 -0500

    Fix not detecting trailing characters bug

--- char_segmentation.py
+++ char_segmentation.py
@@ -67,47 +67,49 @@ def get_chars(img):
                 chars.append(validContr)
         charsInRows.append(chars)
 
-    # Draw countours for testing
+
+    # Sort characters
+    for a in range(len(charsInRows)):
+        charsInRows[a], tempBoundingBoxes = imutil.sort_contours(charsInRows[a])
+
+        # Detect character i and merge it (threshold +-2px)
+        charBoundingBoxes = []
+        b = 0
+        lastI = 0
+        while b < len(tempBoundingBoxes) - 1:
+            currBox = tempBoundingBoxes[b]
+            nextBox = tempBoundingBoxes[b + 1]
+            if currBox[0] >= nextBox[0] - 2 and currBox[0] <= nextBox[0] + 2:
+                x = currBox[0]
+                y = min(currBox[1], nextBox[1])
+                w = max(currBox[0] + currBox[2], nextBox[0] + nextBox[2]) - x
+                h = max(currBox[1] + currBox[3], nextBox[1] + nextBox[3]) - y
+                charBoundingBoxes.append((x, y, w, h))
+                lastI = b
+                b += 2
+            else:
+                charBoundingBoxes.append(currBox)
+                b += 1
+
+        print lastI
+        print len(tempBoundingBoxes) - 2
+        if lastI != len(tempBoundingBoxes) - 2:
+            charBoundingBoxes.append(tempBoundingBoxes[len(tempBoundingBoxes) - 1])
+        charsInRows[a] = charBoundingBoxes
+
+    # Draw bounding boxes for rows
     for row in charsInRows:
-        cv2.drawContours(paper, row, -1, (255, 0, 0))
+        for boundingBox in row:
+            a, b, w, h = boundingBox
+            cv2.rectangle(paper, (a, b), (a+w, b+h), (0, 255, 0), 1)
     show_img(paper)
-#
-#     # Sort characters
-#     for a in range(len(charsInRows)):
-#         charsInRows[a], tempBoundingBoxes = imutil.sort_contours(charsInRows[a])
-#         charBoundingBoxes = []
-#
-#         # Detect character i and merge it (threshold +-2px)
-#         b = 0
-#         while b < len(tempBoundingBoxes) - 1:
-#             currBox = tempBoundingBoxes[b]
-#             nextBox = tempBoundingBoxes[b + 1]
-#             if currBox[0] >= nextBox[0] - 2 and currBox[0] <= nextBox[0] + 2:
-#                 x = currBox[0]
-#                 y = min(currBox[1], nextBox[1])
-#                 w = max(currBox[0] + currBox[2], nextBox[0] + nextBox[2]) - x
-#                 h = max(currBox[1] + currBox[3], nextBox[1] + nextBox[3]) - y
-#                 charBoundingBoxes.append((x, y, w, h))
-#                 b += 2
-#             else:
-#                 charBoundingBoxes.append(currBox)
-#                 b += 1
-#
-#         charsInRows[a] = charBoundingBoxes
-#
-#     # Draw bounding boxes for rows
-#     for row in charsInRows:
-#         for boundingBox in row:
-#             a, b, w, h = boundingBox
-#             cv2.rectangle(paper, (a, b), (a+w, b+h), (0, 255, 0), 1)
-#     show_img(paper)
-#
-#     for boundingBox in rowBoundingBoxes:
-#         a, b, w, h = boundingBox
-#         cv2.rectangle(paper, (a, b), (a+w, b+h), (0, 255, 0), 1)
-#     show_img(paper)
-#
-#     return charsInRows
-#
+
+    for boundingBox in rowBoundingBoxes:
+        a, b, w, h = boundingBox
+        cv2.rectangle(paper, (a, b), (a+w, b+h), (0, 255, 0), 1)
+    show_img(paper)
+
+    return charsInRows
+
 img = cv2.imread("assets/ipsum.jpg")
 get_chars(img)
