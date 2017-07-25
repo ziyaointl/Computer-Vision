dd204cee5ed4b64c009ec66dba96a1bc44fb0f27
smithe3 <smithe3@cmc306-12.mathcs.carleton.edu>
2017-07-25 14:00:19 -0500

    Letter Recog

--- Letter_Recognition.py
+++ Letter_Recognition.py
@@ -3,28 +3,63 @@ __author__ = 'smithe3'
 import cv2
 import numpy as np
 
-doc = open("DataSet.txt", "r")
-letters = doc.read()
+doc = open("assets/DataSet.txt", "r")
+#letters = doc.read()
 
 labels = []
+rawData = []
+data = []
 types = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
          "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-for i in range(1, 63):
-    for j in range(1, 56):
+
+for i in range(62):
+    for j in range(55):
         labels.append(i)
 
+counter = 0
+for i in range(62):
+    for j in range(55):
+        temp = ""
+        for k in range(23):
+            temp += doc.readline()
+            counter = counter+1
 
-data = np.array(letters)
+        if (len(temp)!=1644):
+            print "length " + str(len(temp))
+            print "counter position " + str(counter)
+            print
+        #print len(temp)
+        rawData.append(temp)
+
+for i in range(len(rawData)):
+    temp = rawData[i].split()
+    for j in range(len(temp)):
+        temp[j] = int(temp[j])
+    #temp = np.array(temp)
+    #temp = np.float32(temp)
+    #if(len(temp) != 400):
+        #print temp
+    data.append(temp)
+
+
+#print data
+data = np.array(data)
 labels = np.array(labels)
-data = np.float32(data)
+#data = np.float32(data)
+
+print labels.shape
+print type(labels[0])
+
+print data.shape
+print type(data[0][0])
 
 knn = cv2.ml.KNearest_create()
 knn.train(data, cv2.ml.ROW_SAMPLE, labels)
-ret, result, neighbours, dist = knn.findNearest(letters, k=5)
+ret, result, neighbours, dist = knn.findNearest(data, k=5)
 
 correct = np.count_nonzero(result == labels)
 accuracy = correct*100.0/10000
-print(accuracy)
+#print(accuracy)
 
 
