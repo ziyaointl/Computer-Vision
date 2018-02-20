98a651c364c065a8f8ccfc71ad8ad0bd9d463f37
Ziyao Zhang <ziyaointl@gmail.com>
2018-02-20 18:58:43 +0800

    Implement find_closest()

--- k_means.py
+++ k_means.py
@@ -9,3 +9,12 @@ def contour_center(cnt):
     x = int(rect[0] + .5 * rect[2])
     y = int(rect[1] + .5 * rect[3])
     return x, y
+
+
+def find_closest(pt, centroids, distance_fn):
+    """Return the centroid closest to pt using the distance measured by distance_fn(pt)
+    >>> find_closest((0, 0), [(0, 1), (0, 2)], horizontal_distance)
+    (0, 1)
+    """
+    return min(centroids, key=lambda x: distance_fn(x, pt))
+
