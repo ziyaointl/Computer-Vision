76a969ec92cb475f30f236d0917d974323e9084b
Ziyao Zhang <ziyaointl@gmail.com>
2018-02-21 22:00:30 +0800

    Implement compute_accuracy()

--- tester.py
+++ tester.py
@@ -1,5 +1,14 @@
 from main import find_answers
 
+def compute_accuracy(original_ans, detected_ans):
+    correct = 0.0
+    for i in range(len(original_ans)):
+        if original_ans[i] == detected_ans[i]:
+            correct += 1
+        else:
+            print(i, original_ans[i], detected_ans[i])
+    return correct / len(original_ans)
+
 # filenames = ["assets/IMG_" + num + ".JPG" for num in ['0232','5361', '4783','4203', '5485', '5785', '8083', '9001']]
 
 filenames = ["assets/IMG_" + num + ".JPG" for num in ['2588', '0739', '1071', '1522', '1982', '2434',
@@ -13,3 +22,4 @@ original_ans = ['A', 'C', 'B', 'D', 'B', 'A', 'D', 'B', 'C', 'C', 'A', 'A', 'A',
 for file in filenames:
     print(file)
     detected_ans = find_answers(file)
+    print compute_accuracy(original_ans, detected_ans)
