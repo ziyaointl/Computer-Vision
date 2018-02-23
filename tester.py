7bfaf02e9b99c84683a4955521494b11a81a7657
Ziyao Zhang <ziyaointl@gmail.com>
2018-02-23 19:45:19 +0800

    Improve filename listing

--- tester.py
+++ tester.py
@@ -10,11 +10,12 @@ def compute_accuracy(original_ans, detected_ans):
             print(i + 1, original_ans[i], detected_ans[i])
     return correct / len(original_ans)
 
-filenames = ["assets/IMG_" + num + ".JPG" for num in ['6388', '2826','1243', '3064', '6591', '8836', '8760', '6024', '6112', '1738',
-                                                        '3031', '5241', '7230', '7394', '3497', '6415']]
-
+normal = ['6388', '2826','1243', '3064', '6591', '8836', '8760', '6024',
+          '6112', '1738', '3031', '5241', '7230', '7394', '3497', '6415']
 fails = ['9891', '8880']
 multiple_bubbles = ["1299"]
+
+filenames = ["assets/IMG_" + num + ".JPG" for num in normal]
 
 ORIGINAL_ANS = ['A', 'C', 'B', 'D', 'B', 'A', 'D', 'B', 'C', 'C', 'A', 'A', 'A',
                 'A', 'D', 'C', 'A', 'D', 'D', 'B', 'B', 'A', 'C', 'C', 'C', 'B',
