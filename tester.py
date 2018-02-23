737517e998fe31819b07edfc62c25c554d1fe514
Ziyao Zhang <ziyaointl@gmail.com>
2018-02-23 19:59:01 +0800

    Add documentation for tester.py

--- tester.py
+++ tester.py
@@ -2,6 +2,11 @@ from main import find_answers
 from custom_exceptions import PaperDetectionError, BubbleDetectionError
 
 def compute_accuracy(original_ans, detected_ans):
+    """Returns the accuracy calculated by comparing to lists of answers
+    Upon encountering a wrong answer, this function will print out:
+    (question number, true answer, detected answer)
+    For instance: (40, 'B', 'A')
+    """
     correct = 0.0
     for i in range(len(original_ans)):
         if original_ans[i] == detected_ans[i]:
@@ -10,13 +15,18 @@ def compute_accuracy(original_ans, detected_ans):
             print(i + 1, original_ans[i], detected_ans[i])
     return correct / len(original_ans)
 
+# Normally functional data set
 normal = ['6388', '2826','1243', '3064', '6591', '8836', '8760', '6024',
           '6112', '1738', '3031', '5241', '7230', '7394', '3497', '6415']
+# Filez(s) that cause an exception to be thrown, meaning that the user has to retake an image
 fails = ['9891', '8880']
+# File(s) that contain questions with multiple bubbled in answers. The algorithm still functions normally though,
+# identifying the selected answer for those questions as ''
 multiple_bubbles = ["1299"]
 
 filenames = ["assets/IMG_" + num + ".JPG" for num in normal]
 
+# Answers inputted manually for comparison
 ORIGINAL_ANS = ['A', 'C', 'B', 'D', 'B', 'A', 'D', 'B', 'C', 'C', 'A', 'A', 'A',
                 'A', 'D', 'C', 'A', 'D', 'D', 'B', 'B', 'A', 'C', 'C', 'C', 'B',
                 'B', 'D', 'A', 'B', 'A', 'C', 'D', 'B', 'D', 'D', 'C', 'B', 'A',
@@ -24,6 +34,7 @@ ORIGINAL_ANS = ['A', 'C', 'B', 'D', 'B', 'A', 'D', 'B', 'C', 'C', 'A', 'A', 'A',
 
 cumulative_accuracy = 0
 
+# Loop through each file in list filenames, test the accuracy of find_answers() and print out the cumulative accuracy
 for file in filenames:
     print(file)
     try:
